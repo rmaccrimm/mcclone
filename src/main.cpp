b0b28fd8e347c552bcea76c5c7d5a8ad0f28ebeb
Roderick MacCrimmon <r.maccrimm@gmail.com>
2021-03-15 19:34:32 -0600

    attempt to draw a cube

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 #include <SDL2/SDL.h>
 #include <GL/glew.h>
 #include <stdio.h>
+#include <glm/mat4x4.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/ext/scalar_constants.hpp>
 #include "logo.h"
 
 SDL_Window    *m_window;
@@ -23,98 +28,143 @@ int InitTextures();
 
 const char *vert_shader_src = "\
 #version 150 core                                                            \n\
-in vec2 in_Position;                                                         \n\
-in vec2 in_Texcoord;                                                         \n\
-out vec2 Texcoord;                                                           \n\
+in vec3 in_Position;                                                         \n\
+//in vec2 in_Texcoord;                                                       \n\
+//out vec2 Texcoord;                                                         \n\
+uniform mat4 View;                                                           \n\
+uniform mat4 Projection;                                                     \n\
 void main()                                                                  \n\
 {                                                                            \n\
-    Texcoord = in_Texcoord;                                                  \n\
-    gl_Position = vec4(in_Position, 0.0, 1.0);                               \n\
+    //Texcoord = in_Texcoord;                                                \n\
+    gl_Position = Projection * View * vec4(in_Position, 1.0);                \n\
 }                                                                            \n\
 ";
 
 const char *frag_shader_src = "\
 #version 150 core                                                            \n\
-in vec2 Texcoord;                                                            \n\
-out vec4 out_Color;                                                          \n\
-uniform sampler2D tex;                                                       \n\
+//in vec2 Texcoord;                                                          \n\
+out vec3 out_Color;                                                          \n\
+//uniform sampler2D tex;                                                       \n\
 void main()                                                                  \n\
 {                                                                            \n\
-    out_Color = texture(tex, Texcoord);                                      \n\
+    //out_Color = texture(tex, Texcoord);                                    \n\
+    out_Color = vec3(0.0, 1.0, 0.0);                                         \n\
 }                                                                            \n\
 ";
 
-const GLfloat verts[6][4] = {
-    //  x      y      s      t
-    { -1.0f, -1.0f,  0.0f,  1.0f }, // BL
-    { -1.0f,  1.0f,  0.0f,  0.0f }, // TL
-    {  1.0f,  1.0f,  1.0f,  0.0f }, // TR
-    {  1.0f, -1.0f,  1.0f,  1.0f }, // BR
+const GLfloat cube[24][3] = {
+    // x = 0 face (left)
+    {0, 0, 0},  // BL
+    {0, 0, 1},  // BR
+    {0, 1, 0},  // TL
+    {0, 1, 1},  // TR
+    // x = 1 face (right)
+    {1, 0, 1},  // BL
+    {1, 0, 0},  // BR 
+    {1, 1, 1},  // TL
+    {1, 1, 0},  // TR
+    // y = 0 face (bottom)
+    {0, 0, 0},  //
+    {1, 0, 0},  //
+    {0, 0, 1},  // 
+    {1, 0, 1},  //
+    // y = 1 face (top)
+    {0, 1, 0},  //
+    {0, 1, 1},  //
+    {1, 1, 1},  //
+    {1, 1, 0},  //
+    // z = 0 face (back)
+    {0, 0, 0},  //
+    {0, 1, 0},  //
+    {1, 0, 0},  //
+    {1, 1, 0},  //
+    // z = 1 face (front)
+    {0, 0, 1},  //
+    {0, 1, 1},  //
+    {1, 0, 1},  //
+    {1, 1, 1}   //
 };
 
-const GLint indicies[] = {
-    0, 1, 2, 0, 2, 3
+const GLint indices[] = {
+    0, 3, 1, 0, 2, 3,
+    4, 7, 5, 4, 6, 7,
+    8, 11, 9, 8, 10, 11,
+    12, 15, 13, 12, 14, 15,
+    16, 19, 17, 16, 18, 19,
+    20, 23, 21, 20, 22, 23
 };
+
+void GLAPIENTRY
+MessageCallback( GLenum source,
+                 GLenum type,
+                 GLuint id,
+                 GLenum severity,
+                 GLsizei length,
+                 const GLchar* message,
+                 const void* userParam )
+{
+    fprintf( stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+	     ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ),
+	     type, severity, message );
+}
 
 /*
  * Basic Initialization
  */
 int Initialize()
 {
-    // Initialize SDL Video
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        fprintf(stderr, "Failed to initialize SDL video\n");
-        return 1;
-    }
+  // Initialize SDL Video
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+      fprintf(stderr, "Failed to initialize SDL video\n");
+      return 1;
+  }
 
-    // Create main window
-    m_window = SDL_CreateWindow(
-        "SDL App",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        640, 480,
-        SDL_WINDOW_OPENGL);
-    if (m_window == NULL) {
-        fprintf(stderr, "Failed to create main window\n");
-        SDL_Quit();
-        return 1;
-    }
+  // Create main window
+  m_window =
+      SDL_CreateWindow("SDL App", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+  if (m_window == NULL) {
+      fprintf(stderr, "Failed to create main window\n");
+      SDL_Quit();
+      return 1;
+  }
 
-    // Initialize rendering context
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-    SDL_GL_SetAttribute(
-        SDL_GL_CONTEXT_PROFILE_MASK,
-        SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  // Initialize rendering context
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-    m_context = SDL_GL_CreateContext(m_window);
-    if (m_context == NULL) {
-        fprintf(stderr, "Failed to create GL context\n");
-        SDL_DestroyWindow(m_window);
-        SDL_Quit();
-        return 1;
-    }
+  m_context = SDL_GL_CreateContext(m_window);
+  if (m_context == NULL) {
+      fprintf(stderr, "Failed to create GL context\n");
+      SDL_DestroyWindow(m_window);
+      SDL_Quit();
+      return 1;
+  }
 
-    SDL_GL_SetSwapInterval(1); // Use VSYNC
+  SDL_GL_SetSwapInterval(1); // Use VSYNC
 
-    // Initialize GL Extension Wrangler (GLEW)
-    GLenum err;
-    glewExperimental = GL_TRUE; // Please expose OpenGL 3.x+ interfaces
-    err = glewInit();
-    if (err != GLEW_OK) {
-        fprintf(stderr, "Failed to init GLEW\n");
-        SDL_GL_DeleteContext(m_context);
-        SDL_DestroyWindow(m_window);
-        SDL_Quit();
-        return 1;
-    }
+  // Initialize GL Extension Wrangler (GLEW)
+  GLenum err;
+  glewExperimental = GL_TRUE; // Please expose OpenGL 3.x+ interfaces
+  err = glewInit();
+  if (err != GLEW_OK) {
+      fprintf(stderr, "Failed to init GLEW\n");
+      SDL_GL_DeleteContext(m_context);
+      SDL_DestroyWindow(m_window);
+      SDL_Quit();
+      return 1;
+  }
 
-    InitShaders();
-    InitGeometry();
-    InitTextures();
+  glEnable( GL_DEBUG_OUTPUT );
+  glDebugMessageCallback( MessageCallback, 0 );
 
-    return 0;
+  InitShaders();
+  InitGeometry();
+  // InitTextures();
+
+  return 0;
 }
 
 /*
@@ -171,22 +221,22 @@ int InitGeometry()
     // Populate vertex buffer
     glGenBuffers(1, &m_vbo);
     glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
 
     // Populate element buffer
     glGenBuffers(1, &m_ebo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Bind vertex position attribute
     GLint pos_attr_loc = glGetAttribLocation(m_shader_prog, "in_Position");
-    glVertexAttribPointer(pos_attr_loc, 2, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat), (void*)0);
+    glVertexAttribPointer(pos_attr_loc, 1, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)0);
     glEnableVertexAttribArray(pos_attr_loc);
 
     // Bind vertex texture coordinate attribute
-    GLint tex_attr_loc = glGetAttribLocation(m_shader_prog, "in_Texcoord");
-    glVertexAttribPointer(tex_attr_loc, 2, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat), (void*)(2*sizeof(GLfloat)));
-    glEnableVertexAttribArray(tex_attr_loc);
+    // GLint tex_attr_loc = glGetAttribLocation(m_shader_prog, "in_Texcoord");
+    // glVertexAttribPointer(tex_attr_loc, 1, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (void*)(2*sizeof(GLfloat)));
+    // glEnableVertexAttribArray(tex_attr_loc);
 
     return 0;
 }
@@ -238,11 +288,28 @@ int Cleanup()
  */
 int Update()
 {
+    glUseProgram(m_shader_prog);
+
+    GLint view_loc = glGetUniformLocation(m_shader_prog, "View");
+    GLint proj_loc = glGetUniformLocation(m_shader_prog, "Projection");
+
+    auto proj = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+    auto view = glm::lookAt(glm::vec3(5.0f, 0.0f, 4.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0));
+    
+    glUniformMatrix4fv(proj_loc, 1, GL_FALSE, glm::value_ptr(proj));
+    glUniformMatrix4fv(view_loc, 1, GL_FALSE, glm::value_ptr(view));
     glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
     SDL_GL_SwapWindow(m_window);
     return 0;
+}
+
+void CheckGL() {
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR) {
+	printf("\n A general  OGL error has occured: %i\n", err);
+    }
 }
 
 /*
@@ -266,8 +333,8 @@ int main(int argc, char *argv[])
                 break;
             }
         }
-
-        Update();
+	Update();
+	// CheckGL();
     }
 
     printf("Exiting...\n");
